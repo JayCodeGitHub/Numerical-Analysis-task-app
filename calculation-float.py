7f9b54ffd83cd440953a7886d38354e3a60bbff8
JayCode Developer <JayCode.contakt@gmail.com>
2024-05-21 19:37:19 +0200

    Refactore calculationFloat function

--- calculation-float.py
+++ calculation-float.py
@@ -4,34 +4,36 @@ from scipy.interpolate import CubicSpline
 from mpmath import iv
 
 
-def calculationFloat():
-
-    # Przedział x (można go dostosować)
-    x_start = iv.mpf(1)
-    x_end = iv.mpf(7)
+def calculationFloat(start, end, derStart, derEnd):
     
+    xStart = iv.mpf(start)
+    xEnd = iv.mpf(end)
+
+
+    # Pochodne na końcach przedziałów
+    derivativeStart = iv.mpf(derStart)    
+    derivativeEnd = iv.mpf(derEnd)
+
+
     # Dane wejściowe - przedziały x i odpowiadające im wartości y
     xValuesFloat = np.array([2, 3, 4, 6])  # Przedziały x (zmienno pozycyjne)
     yValuesFloat = np.array([1, 2, 3, 5])  # Przedziały y (zmienno pozycyjne)
     
-    # Pochodne na końcach przedziałów
-    derivativeStart = iv.mpf(6)
-    derivativeEnd = iv.mpf(14)
     
-    # Tworzenie funkcji sklejanej - zmienno pozycyjna
+    # Tworzenie funkcji sklejanej
     csFloat = CubicSpline(xValuesFloat, yValuesFloat, bc_type=((1, derivativeStart), (1, derivativeEnd)))
     
-    # Wartości na końcach przedziału - zmienno pozycyjna
-    valueStartFloat = csFloat(x_start)
-    valueEndFloat = csFloat(x_end)
+    # Wartości na końcach przedziału
+    valueStartFloat = csFloat(xStart)
+    valueEndFloat = csFloat(xEnd)
     
-    # Współczynniki funkcji sklejanej - zmienno pozycyjna
+    # Współczynniki funkcji sklejanej
     coefficientsFloat = csFloat.c
     
 
     return valueStartFloat, valueEndFloat, coefficientsFloat
 
-valueStartFloat, valueEndFloat, coefficientsFloat = calculationFloat()
+valueStartFloat, valueEndFloat, coefficientsFloat = calculationFloat(start=1, end=7, derStart=6, derEnd=14)
 
 print("Wartość na początku przedziału (zmienno pozycyjna):", valueStartFloat)
 print("Wartość na końcu przedziału (zmienno pozycyjna):", valueEndFloat)
