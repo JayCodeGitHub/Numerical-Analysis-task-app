6b7521289e9ba0524654dfefdd28b1f443433755
JayCode Developer <JayCode.contakt@gmail.com>
2024-05-21 19:54:25 +0200

    Refactore calculation function by moving Values outside of function

--- calculation.py
+++ calculation.py
@@ -2,36 +2,45 @@ import numpy as np
 from scipy.interpolate import CubicSpline
 from mpmath import iv
 
-def calculateRanges(start, end):
+xValues = [(1.9, 2.1), (2.9, 3.1), (3.9, 4.1), (5.9, 6.1)]  # Przedziały x
+yValues = [(0.9, 1.1), (1.9, 2.1), (2.9, 3.1), (4.9, 5.1)]  # Przedziały y
 
 
-    # Przedział x (można go dostosować)
+def calculateRanges(start, end, xValues, yValues):
+
+
+    # Przedział x
     xStart = iv.mpf(start)
     xEnd = iv.mpf(end)
 
-    xValuesInterval = np.array([(1.9, 2.1), (2.9, 3.1), (3.9, 4.1), (5.9, 6.1)])  # Przedziały x (przedziałowe)
-    yValuesInterval = np.array([(0.9, 1.1), (1.9, 2.1), (2.9, 3.1), (4.9, 5.1)])  # Przedziały y (przedziałowe)
+    xValuesInterval = np.array(xValues)  # Przedziały x
+    yValuesInterval = np.array(yValues)  # Przedziały y
 
     # Pochodne na końcach przedziałów
     derivativeStart = iv.mpf(6)
     derivativeEnd = iv.mpf(14)
 
-    # Tworzenie funkcji sklejanej - przedziałowa
+    # Tworzenie funkcji sklejanej
     csInterval = CubicSpline(xValuesInterval[:, 0], yValuesInterval[:, 0], bc_type=((1, derivativeStart), (1, derivativeEnd)))
 
-    # Wartości na końcach przedziału - przedziałowa
+    # Wartości na końcach przedziału
     valueStartInterval = csInterval(iv.mpf(xStart))
     valueEndInterval = csInterval(iv.mpf(xEnd))
 
 
-    # Współczynniki funkcji sklejanej - przedziałowa
+    # Współczynniki funkcji sklejanej
     coefficientsInterval = csInterval.c
 
 
     return valueStartInterval, valueEndInterval, coefficientsInterval
 
 
-valueStartInterval, valueEndInterval, coefficientsInterval = calculateRanges(start=1, end=7)
+valueStartInterval, valueEndInterval, coefficientsInterval = calculateRanges(
+    start=1,
+    end=7,
+    xValues=xValues,
+    yValues=yValues
+)
 
 
 
