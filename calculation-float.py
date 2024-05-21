78b73bfc12e18adbfd912639b045f5e60cb458e4
JayCode Developer <JayCode.contakt@gmail.com>
2024-05-21 17:47:02 +0200

    Refactore calculationFloat function

--- calculation-float.py
+++ calculation-float.py
@@ -3,29 +3,36 @@ import matplotlib.pyplot as plt
 from scipy.interpolate import CubicSpline
 from mpmath import iv
 
-# Przedział x (można go dostosować)
-x_start = iv.mpf(1)
-x_end = iv.mpf(7)
 
-# Dane wejściowe - przedziały x i odpowiadające im wartości y
-xValuesFloat = np.array([2, 3, 4, 6])  # Przedziały x (zmienno pozycyjne)
-yValuesFloat = np.array([1, 2, 3, 5])  # Przedziały y (zmienno pozycyjne)
+def calculationFloat():
 
-# Pochodne na końcach przedziałów
-derivativeStart = iv.mpf(6)
-derivativeEnd = iv.mpf(14)
+    # Przedział x (można go dostosować)
+    x_start = iv.mpf(1)
+    x_end = iv.mpf(7)
+    
+    # Dane wejściowe - przedziały x i odpowiadające im wartości y
+    xValuesFloat = np.array([2, 3, 4, 6])  # Przedziały x (zmienno pozycyjne)
+    yValuesFloat = np.array([1, 2, 3, 5])  # Przedziały y (zmienno pozycyjne)
+    
+    # Pochodne na końcach przedziałów
+    derivativeStart = iv.mpf(6)
+    derivativeEnd = iv.mpf(14)
+    
+    # Tworzenie funkcji sklejanej - zmienno pozycyjna
+    csFloat = CubicSpline(xValuesFloat, yValuesFloat, bc_type=((1, derivativeStart), (1, derivativeEnd)))
+    
+    # Wartości na końcach przedziału - zmienno pozycyjna
+    valueStartFloat = csFloat(x_start)
+    valueEndFloat = csFloat(x_end)
+    
+    # Współczynniki funkcji sklejanej - zmienno pozycyjna
+    coefficientsFloat = csFloat.c
+    
 
-# Tworzenie funkcji sklejanej - zmienno pozycyjna
-csFloat = CubicSpline(xValuesFloat, yValuesFloat, bc_type=((1, derivativeStart), (1, derivativeEnd)))
+    return valueStartFloat, valueEndFloat, coefficientsFloat
 
-# Wartości na końcach przedziału - zmienno pozycyjna
-valueStartFloat = csFloat(x_start)
-valueEndFloat = csFloat(x_end)
-
-# Współczynniki funkcji sklejanej - zmienno pozycyjna
-coefficientsFloat = csFloat.c
+valueStartFloat, valueEndFloat, coefficientsFloat = calculationFloat()
 
 print("Wartość na początku przedziału (zmienno pozycyjna):", valueStartFloat)
 print("Wartość na końcu przedziału (zmienno pozycyjna):", valueEndFloat)
 print("Współczynniki funkcji sklejanej (zmienno pozycyjna):", coefficientsFloat)
-
