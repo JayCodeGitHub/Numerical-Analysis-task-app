9d53d3a16ddb13163ba56723aebe9ba8831dae6e
JayCode Developer <JayCode.contakt@gmail.com>
2024-06-01 08:26:21 +0200

    Integrate validation option 1 with GUI

--- window.py
+++ window.py
@@ -3,6 +3,7 @@ from interval import interval
 
 from calculation import calculate
 from calculationInterval import calculateInterval
+from validation import validation
 
 xValuesInterval = [interval[16.9, 17.1], interval[19.9, 20.1], interval[22.9, 23.1], interval[23.9, 24.1], interval[24.9, 25.1], interval[26.9, 27.1], interval[27.6, 27.8]]
 yValuesInterval = [interval[4.4, 4.6], interval[6.9, 7.1], interval[6.0, 6.2], interval[5.5, 5.7], interval[5.7, 5.9], interval[5.1, 5.3], interval[4.0, 4.2]]
@@ -13,8 +14,6 @@ xValuesFloat = [17, 20, 23, 24, 25, 27, 27.7]
 yValuesFloat = [4.5, 7.0, 6.1, 5.6, 5.8, 5.2, 4.1]
 f1x0Float = 3.0
 f1xnFloat = -4.0
-
-xiInput = 23.5
 
 def window():
     root = tk.Tk()
@@ -58,90 +57,105 @@ def window():
     xiInputEntry = tk.Label(root, text="Punk interpolacji (x)")
     xiInputEntry.pack(padx=10, pady=10)
 
-    rangeEntry = tk.Entry(root)
-    rangeEntry.pack()
+    xiEntry = tk.Entry(root)
+    xiEntry.pack()
 
     button = tk.Button(root, text="Wykonaj Obliczenia", command=lambda: runCalculate())
     button.pack(padx=15, pady=15)
 
     def runCalculate():
         if radio_var.get() == "Option 1":
-            c, Value, n, xi = calculate(
-                x = xValuesFloat,
-                f = yValuesFloat,
-                f1x0 = f1x0Float,
-                f1xn = f1xnFloat,
-                xi = xiInput
-            )
+            if(validation(xiEntry.get(), 1)):
+                xiInput = float(xiEntry.get())
+                c, Value, n, xi = calculate(
+                    x = xValuesFloat,
+                    f = yValuesFloat,
+                    f1x0 = f1x0Float,
+                    f1xn = f1xnFloat,
+                    xi = xiInput
+                )
 
-            info.delete('1.0', tk.END)
+                info.delete('1.0', tk.END)
 
-            title = f"Wartość w punkcie {xi}:\n\n{Value:.14e}"
-            subTitle = "Współczynniki:"
+                title = f"Wartość w punkcie {xi}:\n\n{Value:.14e}"
+                subTitle = "Współczynniki:"
 
-            value = f"{title}\n\n\n{subTitle}\n\n"
-            info.insert(tk.END, value)
+                value = f"{title}\n\n\n{subTitle}\n\n"
+                info.insert(tk.END, value)
 
-            for i in range(n):
-                for j in range(4):
-                    if(c[j][i] > 0 ):
-                        info.insert(tk.END, f"a[{j},{i}] =  {c[j][i]:.14e}\n")
-                    else:
-                        info.insert(tk.END, f"a[{j},{i}] = {c[j][i]:.14e}\n")
+                for i in range(n):
+                    for j in range(4):
+                        if(c[j][i] > 0 ):
+                            info.insert(tk.END, f"a[{j},{i}] =  {c[j][i]:.14e}\n")
+                        else:
+                            info.insert(tk.END, f"a[{j},{i}] = {c[j][i]:.14e}\n")
+            else:
+                info.delete('1.0', tk.END)
+                info.insert(tk.END, "Wprowadzono błędne dane")
         elif radio_var.get() == "Option 2":
-            c, Value, n, xi = calculateInterval(
-                x = xValuesInterval,
-                f = yValuesInterval,
-                f1x0 = f1x0Interval,
-                f1xn = f1xnInterval,
-                xi = xiInput
-            )
+            if(validation(xiEntry.get(), 1)):
+                xiInput = float(xiEntry.get())
+                c, Value, n, xi = calculateInterval(
+                    x = xValuesInterval,
+                    f = yValuesInterval,
+                    f1x0 = f1x0Interval,
+                    f1xn = f1xnInterval,
+                    xi = xiInput
+                )
 
-            info.delete('1.0', tk.END)
+                info.delete('1.0', tk.END)
 
-            title = f"Wartość w punkcie {xi}:\n\n({Value[0]:.14e}, {Value[1]:.14e})"
-            subTitle = "Współczynniki:"
+                title = f"Wartość w punkcie {xi}:\n\n({Value[0]:.14e}, {Value[1]:.14e})"
+                subTitle = "Współczynniki:"
 
-            value = f"{title}\n\n\n{subTitle}\n\n"
-            info.insert(tk.END, value)
+                value = f"{title}\n\n\n{subTitle}\n\n"
+                info.insert(tk.END, value)
 
-            for i in range(n):
-                for j in range(4):
-                    if(c[0][j][i] > 0 and  c[1][j][i] > 0):
-                        info.insert(tk.END, f"a[{j},{i}] = (  {c[0][j][i]:.14e},  {c[1][j][i]:.14e} )\n")
-                    elif(c[0][j][i] < 0 and  c[1][j][i] > 0):
-                        info.insert(tk.END, f"a[{j},{i}] = ({c[0][j][i]:.14e}, {c[1][j][i]:.14e})\n")
-                    elif(c[0][j][i] > 0 and  c[1][j][i] < 0):
-                        info.insert(tk.END, f"a[{j},{i}] = ({c[0][j][i]:.14e}, {c[1][j][i]:.14e})\n")
-                    else:
-                        info.insert(tk.END, f"a[{j},{i}] = ( {c[0][j][i]:.14e}, {c[1][j][i]:.14e} )\n")
+                for i in range(n):
+                    for j in range(4):
+                        if(c[0][j][i] > 0 and  c[1][j][i] > 0):
+                            info.insert(tk.END, f"a[{j},{i}] = (  {c[0][j][i]:.14e},  {c[1][j][i]:.14e} )\n")
+                        elif(c[0][j][i] < 0 and  c[1][j][i] > 0):
+                            info.insert(tk.END, f"a[{j},{i}] = ({c[0][j][i]:.14e}, {c[1][j][i]:.14e})\n")
+                        elif(c[0][j][i] > 0 and  c[1][j][i] < 0):
+                            info.insert(tk.END, f"a[{j},{i}] = ({c[0][j][i]:.14e}, {c[1][j][i]:.14e})\n")
+                        else:
+                            info.insert(tk.END, f"a[{j},{i}] = ( {c[0][j][i]:.14e}, {c[1][j][i]:.14e} )\n")
+            else:
+                info.delete('1.0', tk.END)
+                info.insert(tk.END, "Wprowadzono błędne dane")
         elif radio_var.get() == "Option 3":
-            c, Value, n, xi = calculateInterval(
-                x = xValuesInterval,
-                f = yValuesInterval,
-                f1x0 = f1x0Interval,
-                f1xn = f1xnInterval,
-                xi = xiInput
-            )
+            if(validation(xiEntry.get(), 1)):
+                xiInput = float(xiEntry.get())
+                c, Value, n, xi = calculateInterval(
+                    x = xValuesInterval,
+                    f = yValuesInterval,
+                    f1x0 = f1x0Interval,
+                    f1xn = f1xnInterval,
+                    xi = xiInput
+                )
 
-            info.delete('1.0', tk.END)
+                info.delete('1.0', tk.END)
 
-            title = f"Wartość w punkcie {xi}:\n\n({Value[0]:.14e}, {Value[1]:.14e})"
-            subTitle = "Współczynniki:"
+                title = f"Wartość w punkcie {xi}:\n\n({Value[0]:.14e}, {Value[1]:.14e})"
+                subTitle = "Współczynniki:"
 
-            value = f"{title}\n\n\n{subTitle}\n\n"
-            info.insert(tk.END, value)
+                value = f"{title}\n\n\n{subTitle}\n\n"
+                info.insert(tk.END, value)
 
-            for i in range(n):
-                for j in range(4):
-                    if(c[0][j][i] > 0 and  c[1][j][i] > 0):
-                        info.insert(tk.END, f"a[{j},{i}] = (  {c[0][j][i]:.14e},  {c[1][j][i]:.14e} )\n")
-                    elif(c[0][j][i] < 0 and  c[1][j][i] > 0):
-                        info.insert(tk.END, f"a[{j},{i}] = ({c[0][j][i]:.14e}, {c[1][j][i]:.14e})\n")
-                    elif(c[0][j][i] > 0 and  c[1][j][i] < 0):
-                        info.insert(tk.END, f"a[{j},{i}] = ({c[0][j][i]:.14e}, {c[1][j][i]:.14e})\n")
-                    else:
-                        info.insert(tk.END, f"a[{j},{i}] = ( {c[0][j][i]:.14e}, {c[1][j][i]:.14e} )\n")
+                for i in range(n):
+                    for j in range(4):
+                        if(c[0][j][i] > 0 and  c[1][j][i] > 0):
+                            info.insert(tk.END, f"a[{j},{i}] = (  {c[0][j][i]:.14e},  {c[1][j][i]:.14e} )\n")
+                        elif(c[0][j][i] < 0 and  c[1][j][i] > 0):
+                            info.insert(tk.END, f"a[{j},{i}] = ({c[0][j][i]:.14e}, {c[1][j][i]:.14e})\n")
+                        elif(c[0][j][i] > 0 and  c[1][j][i] < 0):
+                            info.insert(tk.END, f"a[{j},{i}] = ({c[0][j][i]:.14e}, {c[1][j][i]:.14e})\n")
+                        else:
+                            info.insert(tk.END, f"a[{j},{i}] = ( {c[0][j][i]:.14e}, {c[1][j][i]:.14e} )\n")
+            else:
+                info.delete('1.0', tk.END)
+                info.insert(tk.END, "Wprowadzono błędne dane")
 
     root.mainloop()
 
